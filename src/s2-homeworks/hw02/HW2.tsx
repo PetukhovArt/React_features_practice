f18278c29a937e0cad557a4744c030210a322326
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-02-10 18:31:06 +0500

    docs: add deploy

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -3,22 +3,6 @@ import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 import {strict} from 'assert';
 
-/*
-* 1 - описать типы AffairPriorityType, AffairType  OK
-* 2 - указать нужный тип для defaultAffairs  OK
-* 3 - дописать типы и логику функции filterAffairs и проверить её тестами ОК
-* 4 - выполнить пункт 3 для функции deleteAffair ОК
-* 5 - указать нужный тип в useState с affairs  OK
-* 6 - дописать тип и логику функции deleteAffairCallback ОК
-*
-* 7 - в файле Affairs.tsx дописать типизацию пропсов OK
-* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow ОК
-*
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
-* */
-
 // types
 export type AffairPriorityType = 'high' | 'low' | 'middle'
 export type AffairType = {
