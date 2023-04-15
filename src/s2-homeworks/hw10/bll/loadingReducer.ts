a5888732e9566ae2619acd9112bdb3d473f88a1a
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-04-15 21:46:20 +0500

    feat: add 10 lesson loader

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,11 @@
-const initState = {
-    isLoading: false,
-}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const initState = {isLoading: false}
+
+export type isLoadingStateType = {isLoading: boolean}
+
+export const loadingReducer = (state = initState, action: LoadingActionType): isLoadingStateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING': return {...state, isLoading: !state.isLoading}
         default:
             return state
     }
