ec09b58f1355b9992d8c9f38c4c13114b2c75392
Artyom <50843331+PetukhovArt@users.noreply.github.com>
2023-04-16 20:59:39 +0500

    feat: HW11 done slider

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -14,9 +14,23 @@ function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
-
-    const change = (event: any, value: any) => {
+    const minDistance = 10;
+    const change = (event: Event, value: number | number[], activeThumb: number) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (Array.isArray(value)) {
+            console.log(activeThumb)
+            // value является массивом
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            // value не является массивом
+            setValue1(value)
+        }
+        // if (activeThumb === 0) {
+        //     setValue2([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+        // } else {
+        //     setValue2([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+        // }
     }
 
     return (
@@ -30,15 +44,19 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={change}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={change}
+                            value={[value1,value2]}
+                            valueLabelDisplay="auto"
+                            disableSwap
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
